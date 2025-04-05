910bf06e982777c7d3cfed21101c1f9c3e90fead
EinsPommes <jannikkugler2006@gmail.com>
2025-04-05 15:06:13 +0200

    Fix ESLint Konfiguration und Lint-Skript

--- backend/eslint.config.js
+++ backend/eslint.config.js
@@ -1,21 +1,20 @@
-import globals from 'globals';
+// ESLint v9 Flat Config
+const globals = require('globals');
 
-export default [
-  {
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'module',
-      globals: {
-        ...globals.node,
-        ...globals.jest
-      }
-    },
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
-    },
-    rules: {
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
+module.exports = {
+  languageOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    globals: {
+      ...globals.node,
+      ...globals.jest
     }
+  },
+  linterOptions: {
+    reportUnusedDisableDirectives: true,
+  },
+  rules: {
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
   }
-];
+};
